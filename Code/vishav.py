1f1520b74b4c392536e57286179f1faf05d3ec4c
vishavdhanjal <31722953+vishavdhanjal@users.noreply.github.com>
2020-03-06 15:31:16 +0530

    Update

--- Code/vishav.py
+++ Code/vishav.py
@@ -8,6 +8,6 @@ from load_data import load_from_folder
 from Segmentation import ECG_segment
 
 dataset=load_from_folder()
-s,apd=ECG_segment(dataset,0)
+s,apd=ECG_segment(dataset,channel_num=0)
 
 #Feature Extraction
