8cb63bcb1d9ed7104ba4bf04990eee82c6c79f02
vishavdhanjal <31722953+vishavdhanjal@users.noreply.github.com>
2020-03-06 16:33:04 +0530

    Update vishav.py

--- Code/vishav.py
+++ Code/vishav.py
@@ -4,10 +4,18 @@ Created on Fri Mar  6 12:34:10 2020
 
 @author: Vishavpreet
 """
+import numpy as np
 from load_data import load_from_folder
 from Segmentation import ECG_segment
+from biosppy.signals.ecg import ecg
 
 dataset=load_from_folder()
 s,apd,arp=ECG_segment(dataset,channel_num=0)
 
 #Feature Extraction
+signal=apd[0]
+fs=500
+ts,filtered,rpeaks,templates_ts, templates, heart_rate_ts, heart_rate=ecg(signal=signal, sampling_rate=fs, show=True)
+
+import matplotlib.pyplot as plt
+plt.plot(templates.T)
