9802672291a0345bff76156e544b68fa83afb043
vishavdhanjal <31722953+vishavdhanjal@users.noreply.github.com>
2020-03-06 15:43:54 +0530

    Update gautham.py

--- Code/gautham.py
+++ Code/gautham.py
@@ -6,6 +6,10 @@ from Segmentation import ECG_segment
 
 dataset=load_from_folder()
 
+#: Segemtnation for  first channel per patient
+#: all_preprocessed_signnals (apd)
+#: all_R_peaks (arp)
+
 s,apd,arp=ECG_segment(dataset,channel_num=0)
 
 #Feature Extraction
