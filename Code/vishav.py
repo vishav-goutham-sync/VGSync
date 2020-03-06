c407843dd215fc482c534cb13baf52680b14a915
vishavdhanjal <31722953+vishavdhanjal@users.noreply.github.com>
2020-03-06 17:57:36 +0530

    Update vishav.py

--- Code/vishav.py
+++ Code/vishav.py
@@ -5,6 +5,7 @@ Created on Fri Mar  6 12:34:10 2020
 @author: Vishavpreet
 """
 import numpy as np
+import time
 from load_data import load_from_folder
 from Segmentation import ECG_segment
 from biosppy.signals.ecg import ecg
@@ -12,8 +13,11 @@ import matplotlib.pyplot as plt
 
 
 dataset=load_from_folder()
-s,apd,arp=ECG_segment(dataset,channel_num=0)
-
+start=time.time()
+s,apd,arp=ECG_segment(dataset[0:100],channel_num=0)
+end=time.time()
+duration=end-start
+print(""+str(duration))
 #Feature Extraction
 for i in range(len(apd)):
     signal=apd[i]
